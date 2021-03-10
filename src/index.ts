dcdb6644f060d4f1d80e4fab9607944b0a21bba3
Glenny De Cock <glenny.decock@gmail.com>
2021-03-10 21:15:58 +0100

    Add Jest testing framework

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 // Provide correct source map support for the tanspiled TS
 import "source-map-support/register";
 
-import { sleep } from "./utils";
+import { sleep, sum } from "./utils";
 
 (async (): Promise<void> => {
   await sleep(1000);
+  console.log(sum(1, 3));
 })();
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
 export const sleep = (ms: number): Promise<void> => {
   return new Promise<void>((resolve) => setTimeout(resolve, ms));
 };
+
+export const sum = (...numbers: number[]): number => {
+  return numbers.reduce((acc, curr) => acc + curr, 0);
+};
